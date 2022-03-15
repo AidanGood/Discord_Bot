ba82e2738297c6e5f3b1584553bb81cdb26cd508
AidanGood <aidangood5@gmail.com>
2022-03-15 00:32:15 -0400

    fix brain

--- dadBot.py
+++ dadBot.py
@@ -7,7 +7,7 @@ import time
 import asyncio
 import re
 from transformers import pipeline
-text_generation = pipeline("text-generation", model='gpt2-medium')
+text_generation = pipeline("text-generation", model='gpt2')
 
 load_dotenv()
 
